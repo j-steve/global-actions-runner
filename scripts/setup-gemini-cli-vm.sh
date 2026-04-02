8fc88e61d99b3f954259991a1964a074a767478b
Steve <Steve5805@gmail.com>
2026-04-02 13:33:15 -0700

    Create setup-gemini-cli-vm.sh

---
+++ scripts/setup-gemini-cli-vm.sh
@@ -0,0 +1,15 @@
+sudo apt update
+sudo apt install -y tmux curl
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo npm install -g @google/gemini-cli
+
+# Setup Vertex API for Gemini CLI.
+echo 'export GOOGLE_CLOUD_PROJECT=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/project/project-id")' >> ~/.bashrc
+echo 'export GOOGLE_CLOUD_LOCATION="global"' >> ~/.bashrc
+echo 'export GOOGLE_GENAI_USE_VERTEXAI=true' >> ~/.bashrc
+source ~/.bashrc
+
+# Enable TMUX scrolling.
+echo "set -g mouse on" >> ~/.tmux.conf
+tmux source-file ~/.tmux.conf
